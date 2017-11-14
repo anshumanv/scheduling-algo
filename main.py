f2fddf7f189ab6b59fd1ee836c62dadc7b407ba1
Anshuman <anshu.av97@gmail.com>
2017-11-15 00:04:10 +0530

    Added a file reading function

--- main.py
+++ main.py
@@ -3,31 +3,35 @@ from SJF import sjf
 from fcfs import fcfs
 from rrobin import round_robin as rr
 
-x = input("Enter file name ")	# File containing dataset
-
-process = []	# Array to store processes data
-
-# Try opening the input file
-try:
-	with open(x, "r") as f:
-		process = f.read().splitlines()
-except:
-	print("Error opening file")
-	exit()
-
-n = len(process)
 p = []
-for i in range(n):
-	p.append(process[i].split(' '))
-#print(p) Test print
+
+def readFile():
+	x = input("Enter file name ")	# File containing dataset
+
+	process = []	# Array to store processes data
+
+	# Try opening the input file
+	try:
+		with open(x, "r") as f:
+			process = f.read().splitlines()
+	except:
+		print("Error opening file")
+		exit()
+
+	n = len(process)
+	for i in range(n):
+		p.append(process[i].split(' '))
+	#print(p) Test print
 
 while(True):
 	print("Enter required operation")
 	print("1: FCFS", "2: SJF", "3: RR", "4:EXIT", sep='\n')
 	cmd = input()
 	if cmd == '1':
+		readFile()
 		fcfs(p)
 	elif cmd == '2':
+		readFile()
 		sjf(p)
 	elif cmd == '3':
 		rr()
