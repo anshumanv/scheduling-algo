1f08c2ea965e47493d5775651f6dc9cbd069d3fa
Anshuman <anshu.av97@gmail.com>
2017-11-15 12:15:38 +0530

    Updated main.py

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from SJF import sjf
 from fcfs import fcfs
 from rrobin import round_robin as rr
 
-p = []
+
 
 def readFile():
+	p = []
 	x = input("Enter file name ")	# File containing dataset
 
 	process = []	# Array to store processes data
@@ -17,22 +18,24 @@ def readFile():
 	except:
 		print("Error opening file")
 		exit()
+	finally:
+		f.close()
 
 	n = len(process)
 	for i in range(n):
 		p.append(process[i].split(' '))
 	#print(p) Test print
+	return p
 
 while(True):
 	print("Enter required operation")
 	print("1: FCFS", "2: SJF", "3: RR", "4:EXIT", sep='\n')
 	cmd = input()
 	if cmd == '1':
-		readFile()
-		fcfs(p)
+		fcfs(readFile())
 	elif cmd == '2':
-		readFile()
-		sjf(p)
+		sjf(readFile())
 	elif cmd == '3':
 		rr()
 	else: exit()
+
