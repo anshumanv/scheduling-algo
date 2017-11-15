317cbf17d86a88f68b824d39e6a28aed7126bf61
Anshuman <anshu.av97@gmail.com>
2017-11-15 12:37:49 +0530

    Updated main

--- main.py
+++ main.py
@@ -4,29 +4,29 @@ from fcfs import fcfs
 from rrobin import round_robin as rr
 
 
-
+# Function to read and parse process data
 def readFile():
-	p = []
+	p = []	# List that stores parsed process data(string)
 	x = input("Enter file name ")	# File containing dataset
 
-	process = []	# Array to store processes data
+	process = []	# Array to store raw processes data
 
 	# Try opening the input file
 	try:
 		with open(x, "r") as f:
-			process = f.read().splitlines()
-	except:
+			process = f.read().splitlines() # Split each line from the file and append to process
+	except:	# Exception handling to when file reading fails
 		print("Error opening file")
 		exit()
-	f.close()
-	return p
 
-	n = len(process)
+	n = len(process) # length of process list
 	for i in range(n):
-		p.append(process[i].split(' '))
-	#print(p) Test print
-	
+		p.append(process[i].split(' '))	# Splitting each process line in terms of spaces
 
+	f.close()	# Closing the input file
+	return p 	# Return the parsed process data list
+	
+# Loop to iterate infinitely and execute processes as per given operation
 while(True):
 	print("Enter required operation")
 	print("1: FCFS", "2: SJF", "3: RR", "4:EXIT", sep='\n')
